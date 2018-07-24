69a23ca90d3ffe7a6684f154adeb9bb3c3e67773
Andres <andresddg@gmail.com>
2018-07-24 09:49:50 +0200

    fix(schema): Updated both schemas (app & back)

--- src/__generated__.ts
+++ src/__generated__.ts
@@ -5,14 +5,19 @@ export interface ExtBookRequestInput {
   b_r_id: string,
   book_date: string,
   made_on: string,
+  reminder_sent?: string | null,
   num_pax: number,
   notes?: string | null,
-  phone: string,
-  email: string,
-  name: string,
-  surname: string,
+  contact_info?: BookRequestContactInfoInput | null,
   service: string,
   channel: string,
+};
+
+export interface BookRequestContactInfoInput {
+  name?: string | null,
+  surname?: string | null,
+  email?: string | null,
+  phone?: string | null,
 };
 
 export interface RestaurantBookingInfoQueryVariables {
@@ -37,15 +42,59 @@ export interface RestaurantBookingInfoQuery {
       starts_at: string | null,
       ends_at: string | null,
       booking_config:  {
-        capacity: number | null,
-        closing_time: string | null,
-        in_advance: number | null,
-        online_allowed: number | null,
+        online_allowed: number,
+        booking_freq: string | null,
+        booking_duration: string | null,
+        in_advance_booking: string | null,
+        prepayment: boolean | null,
+        prepayment_min_pax: number | null,
+        prepayment_pax_charge: number | null,
+        no_show: boolean | null,
         no_show_charge: number | null,
-        min_pax: number | null,
-        max_pax: number | null,
+        no_show_min_pax: number | null,
+        no_show_max_pax: number | null,
+        in_advance_cancellation: number | null,
       } | null,
     } > | null,
+  } | null,
+};
+
+export interface RestaurantSpecialOpeningsToSixMonthsQueryVariables {
+  b_r_id: string,
+  month?: number | null,
+  year?: number | null,
+};
+
+export interface RestaurantSpecialOpeningsToSixMonthsQuery {
+  getRestaurantById:  {
+    b_r_id: string,
+    special_openings:  Array< {
+      so_id: string,
+      open_weekdays: Array< string | null >,
+      from: string,
+      to: string | null,
+      services:  Array< {
+        name: string,
+        is_active: boolean,
+        open_weekdays: Array< string | null > | null,
+        starts_at: string | null,
+        ends_at: string | null,
+        booking_config:  {
+          online_allowed: number,
+          booking_freq: string | null,
+          booking_duration: string | null,
+          in_advance_booking: string | null,
+          prepayment: boolean | null,
+          prepayment_min_pax: number | null,
+          prepayment_pax_charge: number | null,
+          no_show: boolean | null,
+          no_show_charge: number | null,
+          no_show_min_pax: number | null,
+          no_show_max_pax: number | null,
+          in_advance_cancellation: number | null,
+        } | null,
+      } | null >,
+    } | null > | null,
   } | null,
 };
 
